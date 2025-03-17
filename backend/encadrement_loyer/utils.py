9d1d6576bcce49936e9930177cd5deb4690a083b
Nicolas3269 <nicolas3269@gmail.com>
2025-03-17 12:22:20 +0100

    fixup: pays basques

---
+++ backend/encadrement_loyer/utils.py
@@ -0,0 +1,14 @@
+import requests
+import os
+GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
+def geocode_address(address):
+    """Convertit une adresse en coordonnées latitude/longitude via Google Maps API"""
+    url = f"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={GOOGLE_MAPS_API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    
+    if data["status"] == "OK":
+        location = data["results"][0]["geometry"]["location"]
+        return location["lat"], location["lng"]
+    else:
+        raise ValueError("Adresse introuvable.")

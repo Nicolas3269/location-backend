85cb072f355f9aaf92d832fbb65295348c2354ae
Nicolas3269 <nicolas3269@gmail.com>
2025-06-13 17:49:56 +0200

    fixup

--- backend/settings.py
+++ backend/settings.py
@@ -145,7 +145,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "fr-fr"
+USE_L10N = True
 
 TIME_ZONE = "UTC"
 
